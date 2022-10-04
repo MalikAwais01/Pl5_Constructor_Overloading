8659846285d71754573f2306b803625910db4b6b
awais malik <awais.malik@usa.edu.pk>
2022-10-04 12:32:41 +0500

    Add project files.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person myobj = new Person(123456, "Shah WAli Ullah", 'A', "DHA Rahbar");
+            Console.WriteLine(myobj.id + " " + myobj.name + " " + myobj.grade + " " + myobj.add);
+
+            Person obj = new Person(45678, "Farris", 'A');
+            Console.WriteLine(obj.id + " " + obj.name + " " + obj.grade );
+
+
+        }
+    }
+}
